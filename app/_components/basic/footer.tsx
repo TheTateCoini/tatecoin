27298bfb7af08ceee431fb64cd21d910692154c8
Nodebe John-Okeke <nodebe@cookie.technology>
2023-08-10 09:39:26 +0100

    footer edit

--- app/_components/basic/footer.tsx
+++ app/_components/basic/footer.tsx
@@ -74,22 +74,6 @@ export default function Footer() {
                 <p className="opacity-60">contact@tatecoin.com</p>
               </div>
             </div>
-            <div className="flex items-center justify-start space-x-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-9 h-9"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.60v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
-                />
-              </svg>
-            </div>
           </div>
         </div>
       </div>
